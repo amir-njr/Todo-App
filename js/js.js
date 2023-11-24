4a2bcf832691da7f0142f0c5d64a51d4c8c4e143
amir-njr <amirr.najjar.78@gmail.com>
2023-11-24 14:13:12 +0330

    Complet Delete Handler

--- js/js.js
+++ js/js.js
@@ -3,8 +3,12 @@ const taskTodo = document.querySelector("#taskTodo");
 const dateTodo = document.querySelector("#dateTodo");
 const alertBox = document.querySelector("#alertMessage");
 const table = document.querySelector("#tbody");
+const deleteAllTodo = document.querySelector("#deleteAllTodo");
 
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+const pending = document.querySelector("#pending-filter");
+const completed = document.querySelector("#completed-filter");
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 function addTodoHandler() {
   const todo = {
@@ -15,7 +19,6 @@ function addTodoHandler() {
   };
 
   if (todo.taskName) {
-    table.innerHTML = "";
     todos.push(todo);
     taskTodo.value = "";
     dateTodo.value = "";
@@ -24,9 +27,11 @@ function addTodoHandler() {
     showAlert("Task Successfuly", "success");
   } else {
     showAlert("Task Faild", "error");
-    // showTask()
+    showTask();
   }
 }
+addTodo.addEventListener("click", addTodoHandler);
+
 function saveToLocalStorage() {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
@@ -48,7 +53,8 @@ function generateId() {
   ).toString();
 }
 function showTask() {
-  console.log(table);
+  table.innerHTML = "";
+
   if (todos.length) {
     todos.forEach((todo) => {
       table.innerHTML += `
@@ -56,12 +62,45 @@ function showTask() {
       <td>${todo.taskName}</td>
       <td>${todo.dateTask ? todo.dateTask : "No Date"}</td>
       <td>${todo.completed ? "Completed" : "Pending"}</td>
-      <td><button>Edit</button><button>Do</button><button>Delete</button></td>
+      <td>
+        <button id="edit">Edit</button>
+        <button id="do">Do</button>
+        <button id="delete">Delete</button>
+      </td>
       </tr>
       `;
     });
   } else {
-    table.innerHTML = "<tr><td>Not Found Task</td></tr>";
+    table.innerHTML = "<tr><td colspan='4'>Not Found Task</td></tr>";
   }
 }
-addTodo.addEventListener("click", addTodoHandler);
+// function filterTask(e) {
+//   const todos = JSON.parse(localStorage.getItem("todos")) || [];
+//   const resultFilter = [];
+//   if (e.target.dataset.pending === "pending") {
+//     todos.forEach((todo) => {
+//       todo.completed === false ? todo.style.display = "": null
+//     });
+
+//   } else if (e.target.dataset.pending === "compeleted") {
+//     todos.forEach((todo) => {
+//       return todo.completed === true;
+//     });
+//   }
+// }
+
+// pending.addEventListener("click", filterTask);
+// completed.addEventListener("click", filterTask);
+
+function deleteAllHandler() {
+  if (todos.length) {
+    todos = [];
+    saveToLocalStorage();
+    showTask()
+    showAlert("All Todos Cleared Successfuly ", "success");
+  } else {
+    showAlert("No Todos to Clear", "error");
+  }
+  localStorage.clear("todos");
+}
+deleteAllTodo.addEventListener("click", deleteAllHandler);
